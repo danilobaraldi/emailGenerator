6dffc8040e4c56342fdb301a63b59f60ebb7a750
Danilo Baraldi <danilobaraldi@hotmail.com>
2022-01-30 13:37:46 -0300

    Function to include new names is done, increased array to a list with 10 nmes, initiate logic to apply filters and generate the mail@company.com

--- script/script.js
+++ script/script.js
@@ -1,5 +1,21 @@
-let listNames = ["Adam Benson", "Clare Donaldson", "Ben Adams", "Donald Clarkson"]; 
+let listNames = ['Elisabe Freitas Dantas','Peter Parker', 'Adam Benson', 'Clare Donaldson', 'Ben Adams', 'Donald Clarkson', 'Amanda Mendes Costa', 'Daniel de Souza Santos', 'Alise de Souza Araujo-Oliveira', 'Marcos Antonio Paiva Filho']; 
 
 function addName(){
-    addEventListener
+    let input = document.getElementById("addEmploye");
+    listNames.push(input.value);
+    console.log(input.value);
+    
+}
+
+function showList(){
+
+    let tableList = document.getElementById("listUsers")
+    for(let i = 0; i <listNames.length; i++){
+
+        let showUser = listNames[i];
+        
+
+    }
+        
+    ;
 }
