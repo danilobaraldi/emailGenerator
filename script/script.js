2b19ecc902732ce4dcab92c0d318b73bc8055d06
Danilo Baraldi <danilobaraldi@hotmail.com>
2022-02-01 19:37:01 -0300

    included table at page, pending task: add counter to e-mails with same description. Resolution in progress

--- script/script.js
+++ script/script.js
@@ -1,52 +1,51 @@
 const input = document.getElementById("addEmployee");
 let listNames = [
-    'Elisabete Freitas-Dantas',
-    'Peter Parker',
-    'Adam Benson',
-    'Davis Clarkson',
-    'Pedro Parker',
-    'Donald Clarkson',
-    'Amanda Mendes Costa',
-    'Daniel de Souza Santos',
-    'Alise de Souza Araujo-Oliveira',
-    'Marcos Antonio Paiva Filho'
+  "Elisabete Freitas-Dantas",
+  "Peter Parker",
+  "Adam Benson",
+  "Davis Clarkson",
+  "Pedro Parker",
+  "Donald Clarkson",
+  "Amanda Mendes Costa",
+  "Daniel de Souza Santos",
+  "Alise de Souza Araujo-Oliveira",
+  "Marcos Antonio Paiva Filho",
 ];
 
 function addName() {
-    
-    if (input.value == '' || input.value == ' ' || input.value.length < 8) {
-        return alert("Insira o nome COMPLETO do funcionário!")   
-    }
+  if (input.value == "" || input.value == " " || input.value.length < 8) {
+    return alert("Insira o nome COMPLETO do funcionário!");
+  }
 
-    let validName = listNames.includes(input.value);
-    if (validName == false) {
-        
-        setTimeout(() => {
-            input.value
-        }, 10);
-        return listNames.push(input.value);
-    } else {
-        return alert("Funcionário já cadastrado!!!")
-    }
+  let validName = listNames.includes(input.value);
+  if (validName === false) {
+    listNames.push(input.value);
+    loadTable();
+  } else {
+    alert("Funcionário já cadastrado!!!");
+  }
 }
 
+const loadTable = () => {
+  document.getElementById("list-of-emails").innerHTML = listNames.map((name) => 
+  `<tr>
+    <td>${name}</td>
+    <td>${getEmail(name)}</td>
+   </tr>`).join("");
+};
 
-const removeHifen = listNames.map((name) => {
-    return name.split('-').join('').toLocaleLowerCase();
-})
+const getEmail = (name) => {
+    let splitName = name.split("-").join("").split(" ");
+  
+    let newName = splitName[splitName.length - 1];
+    for (let index = 0; index < splitName.length - 1; index++) {
+      newName += `.${splitName[index][0][0]}`;
+    }
+  
+    return `${newName}@company.com`.toLocaleLowerCase();
+  };
 
-const returnSubList = removeHifen.map((value)=>{
-    return value.split(' ')
-})
-console.log(returnSubList)
+ 
+loadTable();
 
-const returnMail = returnSubList.map((value,index)=>{
-    return `${value[value.length -1]}.${value[0][0]}@company.com`;
-    
-})
-console.log(returnMail)
-
-//TODO: Adicionar algum tipo de setTimeOut - para fazer o reload do código após a inclusão do nove nome. 
-//TODO: Criar uma regra para validação do nome com base na quantidade dos arrays para gerar o e-mail. 
-//TODO: Exportar os e-mails para uma lista permanente 
 //TODO: Manipular os e-mails e adicionar um contador caso já exista.
