4bba99fe0bf8093bbb898279ec94ef88d83e2dd4
Danilo Baraldi <danilobaraldi@hotmail.com>
2022-02-02 10:26:01 -0300

    able to add counter value before '@', still working to increase value dinamically, this is the last step to complete this projetct

--- script/script.js
+++ script/script.js
@@ -10,7 +10,10 @@ let listNames = [
   "Daniel de Souza Santos",
   "Alise de Souza Araujo-Oliveira",
   "Marcos Antonio Paiva Filho",
+  "Paulo Parker"
 ];
+
+let validEmail = [];
 
 function addName() {
   if (input.value == "" || input.value == " " || input.value.length < 8) {
@@ -27,33 +30,32 @@ function addName() {
 }
 
 const loadTable = () => {
-  document.getElementById("list-of-emails").innerHTML = listNames.map((name) => 
-  `<tr>
-    <td>${name}</td>
-    <td>${getEmail(name)}</td>
-   </tr>`).join("");
+  document.getElementById("list-of-emails").innerHTML = listNames
+    .map((name) =>`<tr>
+                      <td>${name}</td>
+                      <td>${getEmail(name)}</td>
+                    </tr>`).join("");
+
+  validEmail = [];
 };
-let validEmail =[];
+
 const getEmail = (name) => {
-    let splitName = name.split("-").join("").split(" ");
-    let msg ="";
-  
-    let newName = splitName[splitName.length - 1];
-    for (let index = 0; index < splitName.length - 1; index++) {
-      newName += `.${splitName[index][0][0]}`; 
-      
-      if(validEmail.includes(newName)){
-          validEmail = newName;
-      }else{
-          validEmail = newName+1;
-      }
-    } 
-    
-    
-    msg = `${newName}@company.com`.toLocaleLowerCase()
-    console.log(validEmail)
-    //console.log (msg)
-    return msg;
-  };
+  let splitName = name.split("-").join("").split(" ");
+  let msg = "";
+
+  let newName = splitName[splitName.length - 1];
+  for (let index = 0; index < splitName.length - 1; index++) {
+    newName += `.${splitName[index][0][0]}`;
+
+    if (validEmail.includes(newName)) {
+      newName = newName + 1;
+    }
+    validEmail.push(newName);
+  }
+  msg = `${newName}@company.com`.toLocaleLowerCase();
+
+  //console.log (validEmail)
+  return msg;
+};
 
 loadTable();
