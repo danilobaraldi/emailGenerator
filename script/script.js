e4a897e4fb4ec201d7822e17c811a6d37cedec6b
Danilo Baraldi <danilobaraldi@hotmail.com>
2022-02-02 11:59:01 -0300

    completed logic for counter. PROJECT DONE!

--- script/script.js
+++ script/script.js
@@ -2,18 +2,18 @@ const input = document.getElementById("addEmployee");
 let listNames = [
   "Elisabete Freitas-Dantas",
   "Peter Parker",
+  "Peralta Parker",
   "Adam Benson",
   "Davis Clarkson",
+  "David Clarkson",
+  "Davis Clarksonss",
   "Pedro Parker",
   "Donald Clarkson",
   "Amanda Mendes Costa",
   "Daniel de Souza Santos",
-  "Alise de Souza Araujo-Oliveira",
+  "Alice de Souza Araujo-Oliveira",
   "Marcos Antonio Paiva Filho",
-  "Paulo Parker"
 ];
-
-let validEmail = [];
 
 function addName() {
   if (input.value == "" || input.value == " " || input.value.length < 8) {
@@ -30,13 +30,51 @@ function addName() {
 }
 
 const loadTable = () => {
-  document.getElementById("list-of-emails").innerHTML = listNames
-    .map((name) =>`<tr>
-                      <td>${name}</td>
-                      <td>${getEmail(name)}</td>
-                    </tr>`).join("");
+  document.getElementById("list-of-emails").innerHTML = getEmails()
+    .map((cadastro) =>`<tr>
+                          <td>${cadastro.name}</td>
+                          <td>${cadastro.email}@company.com</td>
+                        </tr>`
+    ).join("");
 
-  validEmail = [];
+};
+
+const fixEmails = (arr) => {
+  var outputList = [];
+  let countEmails = 1;
+  let preview = {};
+
+
+  arr = arr.sort((a, b) => {
+    if (a.email < b.email) {
+      return -1;
+    }
+    if (a.email > b.email) {
+      return 1;
+    }
+    return 0;
+  });
+
+  for  (let index = 0; index < arr.length; index++) {
+    if (
+      outputList.map((elem) => elem.email).indexOf(arr[index].email) === -1 &&
+      arr[index].email !== ""
+    ) {
+      outputList.push({ name: arr[index].name, email: arr[index].email });
+    } else {
+      outputList.push({ name: arr[index].name, email: arr[index].email + countEmails });
+    }
+    if (index === 0) {
+      preview = arr[index];
+    }
+    if (index > 0 && arr[index - 1].email !== arr[index].email) {
+      preview = arr[index];
+      countEmails = 0;
+    }
+    countEmails++;
+  }
+
+  return outputList;
 };
 
 const getEmail = (name) => {
@@ -46,16 +84,14 @@ const getEmail = (name) => {
   let newName = splitName[splitName.length - 1];
   for (let index = 0; index < splitName.length - 1; index++) {
     newName += `.${splitName[index][0][0]}`;
-
-    if (validEmail.includes(newName)) {
-      newName = newName + 1;
-    }
-    validEmail.push(newName);
   }
-  msg = `${newName}@company.com`.toLocaleLowerCase();
+  msg = `${newName}`.toLocaleLowerCase();
 
-  //console.log (validEmail)
   return msg;
+};
+
+const getEmails = () => {
+  return fixEmails(listNames.map((name) => ({ name, email: getEmail(name) })));
 };
 
 loadTable();
